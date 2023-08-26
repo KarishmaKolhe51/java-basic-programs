8662aea1b8a5619885388cbf2f496f2c6f760e8a
Karishma Kolhe <karishmakolhe49@gmail.com>
2023-08-26 16:52:54 +0530

    fibonacci series using recursion

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -23,8 +23,26 @@ public class FibonacciSeries {
         }
     }
 
-    public static void main(String args[]) {
+    public static void main2(String args[]) {
         int N = 18;
         Fibonacci(N);
+    }
+
+    static int num1 = 0, num2 = 1, num3 = 0;
+
+    static void printFibonacci(int count) {
+        if (count > 0) {
+            num3 = num1 + num2;
+            num1 = num2;
+            num2 = num3;
+            System.out.print(" " + num3);
+            printFibonacci(count - 1);
+        }
+    }
+
+    public static void main(String args[]) {
+        int count = 10;
+        System.out.print(num1 + " " + num2); // printing 0 and 1
+        printFibonacci(count - 2); // n -2 because2 numbers are already printed
     }
 }
