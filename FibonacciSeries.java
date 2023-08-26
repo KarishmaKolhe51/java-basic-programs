edc3bc17ae75374b554e17c5bd4ccb5db0da9d25
Karishma Kolhe <karishmakolhe49@gmail.com>
2023-08-26 16:54:40 +0530

    update

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -30,7 +30,7 @@ public class FibonacciSeries {
 
     static int num1 = 0, num2 = 1, num3 = 0;
 
-    static void printFibonacci(int count) {
+    static void printFibonacci(int count) { //// time complexity is O(2^N)
         if (count > 0) {
             num3 = num1 + num2;
             num1 = num2;
