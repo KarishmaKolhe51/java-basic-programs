4e11255be195b246c6b18b3a8ee942494d8c6599
Karishma Kolhe <karishmakolhe49@gmail.com>
2023-08-26 15:15:42 +0530

    fibonacci series using iterative/while loop

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,13 +1,30 @@
 public class FibonacciSeries {
-    public static void main(String args[]) {
+    public static void main1(String args[]) {
         int num1 = 0, num2 = 1, num3, count = 10;
-        System.out.print(num1 + " " + num2);
+        System.out.print(num1 + " " + num2); // printing 0 and 1
 
-        for (int i = 2; i < count; ++i) {
+        for (int i = 2; i < count; ++i) { // loop starts from 2 because 0 and 1 are already printed
             num3 = num1 + num2;
             System.out.print(" " + num3);
             num1 = num2;
             num2 = num3;
         }
+    }
+
+    static void Fibonacci(int N) { // time complexity is O(N)
+        int num1 = 0, num2 = 1, counter = 0;
+
+        while (counter < N) {
+            System.out.print(num1 + " ");
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            counter++;
+        }
+    }
+
+    public static void main(String args[]) {
+        int N = 18;
+        Fibonacci(N);
     }
 }
